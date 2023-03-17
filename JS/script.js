29723382f6e5c3e98902872dc10c8956f1601ed3
NathanMotoki <nathan_motoki@hotmail.com>
2023-03-16 23:56:39 -0300

    Fim do Projeto Clima

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,55 @@
-// ac3976b1ab5f5c70fec9cd0a2b2bb9df
+// Keys API e Variaveis
+const apiKey = "ac3976b1ab5f5c70fec9cd0a2b2bb9df";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+const weatherContainer = document.querySelector("#weather-data");
+
+// Funções
+
+const getWeatherData = async(city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    return data;
+}
+
+const showWeatherData = async(city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+    weatherContainer.classList.remove("hide");
+}
+
+// Eventos
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const city = cityInput.value;
+    showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        const city = e.target.value;
+        showWeatherData(city);
+    }
+});
